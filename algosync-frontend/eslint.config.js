c2878dec15371fdaa38ea7f2e5bc1fe81eb9feeb
xin.c <zhangxinping111@qq.com>
2025-09-04 15:29:29 +0800

    完善配置pre esl classn...

--- algosync-frontend/eslint.config.js
+++ algosync-frontend/eslint.config.js
@@ -1,23 +1,43 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+// eslint.config.js
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import { fixupConfigRules } from '@eslint/compat';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import pluginReactRefresh from 'eslint-plugin-react-refresh';
+import prettierConfig from 'eslint-config-prettier'; // 引入 Prettier 配置
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default [
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: { ...globals.browser, ...globals.node }
+    }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+    ...fixupConfigRules(pluginReactConfig), // 使用 compat 包裹旧版 react/recommended
+    settings: {
+      react: {
+        version: 'detect', // 自动检测 React 版本
+      },
     },
   },
-])
+  {
+    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+    plugins: {
+      'react-hooks': pluginReactHooks,
+      'react-refresh': pluginReactRefresh
+    },
+    rules: {
+      ...pluginReactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': 'warn'
+    }
+  },
+  prettierConfig,
+  {
+    ignores: ['dist', 'node_modules', '.vscode'], // 全局忽略文件
+  }
+];
---
+++ algosync-frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // 匹配 src 下的所有相关文件
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
