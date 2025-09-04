a4ac66483cf3c1c5e0e50c80e1660e146a2d1fc1
xin.c <zhangxinping111@qq.com>
2025-09-04 16:27:14 +0800

    基本结构已经搭建完成

--- algosync-frontend/vite.config.ts
+++ algosync-frontend/vite.config.ts
@@ -1,7 +1,14 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // 引入 path 模块
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src') // 设置别名
+    }
+  }
 })
